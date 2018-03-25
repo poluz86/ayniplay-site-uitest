91152b65aea91d45399e5802f01d439a87a84e8f
Gregory Valderrama <gcvalderrama@hotmail.com>
2018-03-25 17:34:42 -0500

    add

--- src/test/java/ayniplay/steps/CommonSteps.java
+++ src/test/java/ayniplay/steps/CommonSteps.java
@@ -7,13 +7,5 @@ public class CommonSteps {
     public CommonSteps() {
 
 
-
-        Given("^username and login$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new cucumber.api.PendingException();
-
-            return null;
-
-        });
     }
 }
